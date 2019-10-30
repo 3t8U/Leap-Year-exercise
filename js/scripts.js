0bc0ac32ecb7ae3d251526d457083237a23cdbe8
Malcolm Womble <malcolmaw@icloud.com>
2019-10-29 20:39:32 -0700

    add spec that returns true when year is divisible by 4 and not divisible by 100 or divisible by 400

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     event.preventDefault();
 
     var leapYear = function(year) {
-      if (year % 4 === 0) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
         return true;
       } else {
         return false;
